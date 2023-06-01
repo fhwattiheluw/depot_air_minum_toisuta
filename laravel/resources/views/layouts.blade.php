60c752ad18ba875ffeb039fccb9a2e68e0d48674
fhwattiheluw <fadliwattiheluw1994@gmail.com>
2023-06-01 13:44:33 +0900

    layout

--- laravel/resources/views/layouts.blade.php
+++ laravel/resources/views/layouts.blade.php
@@ -128,7 +128,7 @@
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
               <i class="fas fa-table"></i>
             </div>
-            <span class="nav-link-text ms-1">Rekapan semua</span>
+            <span class="nav-link-text ms-1">Rekapan pemasukan</span>
           </a>
         </li>
         <li class="nav-item">
